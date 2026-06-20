a89b4806e6b928ad27f8f6332d2940324cc3a55c
Ryan Costa <89921782+ryanfcosta@users.noreply.github.com>
2026-06-20 14:40:21 -0300

    versao mais atualizada

--- simumic/src/main/java/com/simumic/Cache.java
+++ simumic/src/main/java/com/simumic/Cache.java
@@ -4,6 +4,6 @@ public class Cache extends Memoria{
     // 8B de cache (proporção 1000) 2B 4 linhas 
     // Essa é a cache L1
 
-    private int[] enderecos = new int[4];
+    //private int[] enderecos = new int[4];
 
 }
